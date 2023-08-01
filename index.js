8c40a52ef0e22be2adf4ed61e65a18e2e5fed08b
Kamran Rafiq Sofi <108427136+Kamran-Rafiq-sofi@users.noreply.github.com>
2023-08-01 21:02:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(session({
     store: MongoStore.create({
         // mongoUrl: 'mongodb://127.0.0.1/IssueTrackerApp',
         
-mongoUrl:`mongodb+srv://kamranrafiq805:kamranrafiqsofi@cluster0406.8ritmss.mongodb.net/${env.db}`
+mongoUrl:'mongodb+srv://kamranrafiq805:kamranrafiqsofi@cluster0406.8ritmss.mongodb.net/env.db
    
 
         collectionName: 'session',
