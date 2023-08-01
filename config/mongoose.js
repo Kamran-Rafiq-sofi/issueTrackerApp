7ea103645cd4c172f6dbf2294d6ba925ae059888
Kamran Rafiq Sofi <108427136+Kamran-Rafiq-sofi@users.noreply.github.com>
2023-08-01 20:55:18 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,13 @@
 const env=require('./environment');
 const mongoose=require('mongoose')
-// const url='mongodb://127.0.0.1/IssueTrackerApp';
-// const url=`mongodb://127.0.0.1:27017/${env.db}`;
-// const url=`mongodb://127.0.0.1/${env.db}`;
-// const url=process.env.ERS_DB_URI;
+
 
 
 // mongoose.connect(url);
 console.log(env.db)
 // mongoose.connect(`mongodb://127.0.0.1/${env.db}`);
-mongoose.connect('mongodb+srv://kamranrafiq805:kamranrafiqsofi@cluster0406.8ritmss.mongodb.net/issuetracker')
+
+mongoose.connect(`mongodb+srv://kamranrafiq805:kamranrafiqsofi@cluster0406.8ritmss.mongodb.net/${env.db}`)
 const db=mongoose.connection;
 db.on('error', console.error.bind(console,"error in connecting to db"))
 db.once('open',()=>{
